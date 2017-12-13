fc58978281af17a8ba62ed2d0c9fa5a007006800
Buckner <15410@C02PD3U2GFWM.local>
2017-12-13 10:57:12 -0500

    fixed silly mistake

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main(void)
 {
-	int rps = 62.5; // fps cap (125/2) = 62.5 get over it
+	int rps = 62; // fps cap (125/2) = 62.5 get over it
 	int pps = 125; // processes per second
 	int p = 0; //iterative variable for multitasking stuff
 	init();
